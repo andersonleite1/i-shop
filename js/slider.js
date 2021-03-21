ce793de6d1d09ab6386fb13dd4f6cfba9eedaab9
Anderson Leite <andersonleite.dev@gmail.com>
2021-03-21 20:45:58 -0300

    Create Latest Products

--- js/slider.js
+++ js/slider.js
@@ -5,6 +5,7 @@ Glide Js Carousel
  */
 
 const slider1 = document.getElementById("glide_1");
+const slider2 = document.getElementById("glide_2");
 
 /*
 =============
@@ -20,5 +21,30 @@ if (slider1) {
     perView: 1,
     animationDuration: 800,
     animationTimingFunc: "linear",
+  }).mount();
+}
+
+/*
+=============
+Latest Products
+=============
+ */
+
+if (slider2) {
+  new Glide("#glide_2", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4,
+    rewin: false,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      1200: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+    },
   }).mount();
 }
